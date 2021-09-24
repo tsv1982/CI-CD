1a1595e1b7982b01b672e34efe97f3f37d294a68
Tarabanov Serhii <tsv198250@gmail.com>
2021-09-24 23:42:05 +0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -10,8 +10,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update -y
 sudo apt install -y docker-ce
 sudo usermod -aG docker ubuntu
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/lo>
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo apt update -y
 sudo apt install -y apache2

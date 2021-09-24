ba98feef2ef2f21e753015d01e71e93a99a44d18
Tarabanov Serhii <tsv198250@gmail.com>
2021-09-24 23:17:25 +0300

    Add files via upload

---
+++ user_data.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo git clone https://github.com/tsv1982/CI-CD.git
+
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update -y
+sudo apt install -y docker-ce
+sudo usermod -aG docker ubuntu
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/lo>
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt update -y
+sudo apt install -y apache2
+
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+
+cat <<EOF > /var/www/html/index.html
+<html>
+<body bgcolor="black">
+<h2><font color="gold">Build by Power of Terraform <font color="red"> v0.12</font></h2><br><p>
+<font color="green">Server PrivateIP: <font color="aqua">$myip<br><br>
+
+<font color="magenta">
+<b>Version 3.0</b>
+</body>
+</html>
+
+EOF
+
+sudo systemctl start apache2
+chkconfig httpd on
+
+
+

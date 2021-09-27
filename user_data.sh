093a881ca8229b489966ccce82ca6e6a3162d14a
Tarabanov Serhii <tsv198250@gmail.com>
2021-09-28 00:36:26 +0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-sudo git clone https://github.com/tsv1982/CI-CD.git
-
-sudo apt update -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
-sudo apt update -y
-sudo apt install -y docker-ce
-sudo usermod -aG docker ubuntu
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 sudo apt update -y
 sudo apt install -y apache2
 

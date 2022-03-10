fca72ade9372cd0c058ae04d350234b7f5bab0d6
tsv1982 <tsv198250@gmail.com>
2022-03-10 03:50:23 +0200

    006

--- MainTest.java
+++ MainTest.java
@@ -2,7 +2,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+
 class MainTest {
+
+    @BeforeAll
+    public static void BeforeClass(){System.out.println("TEST TEST TEST");}
+    
+
     @Test
     void calculate() {
         assertEquals(9, new Main().calculate(4,5));
